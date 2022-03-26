0c1f8a4682c5f6e6cf342f45e2c161c0adfe9320
LoneWolfHT <lonewolf04361@gmail.com>
2022-03-26 09:11:57 -0700

    Update Base

--- util/buttonpress.gd
+++ util/buttonpress.gd
@@ -1,5 +1,4 @@
 extends Button
-# test0
 
 export(String) var button_name = "button_name"
 export(String, FILE, "*.tscn") var load_scene
@@ -9,10 +8,12 @@ func _ready():
 	if typeof(signal_target) == TYPE_NODE_PATH:
 		signal_target = get_node(signal_target)
 
-	assert(self.connect("button_up", signal_target, "_on_button_activate", [ button_name ]) == OK, "[util->buttonpress]: Failed to connect signal to given node")
+	if (self.connect("button_up", signal_target, "_on_button_activate", [ button_name ]) != OK):
+		push_error("[util->buttonpress]: Failed to connect signal to given node")
 
 	if load_scene:
-		assert(self.connect("button_up", self, "_on_loadscene_request", [ load_scene ]) == OK, "Failed to connect sceneload func to buttonpress signal")
+		if (self.connect("button_up", self, "_on_loadscene_request", [ load_scene ]) != OK):
+			push_error("Failed to connect sceneload func to buttonpress signal")
 
 	print("[util->buttonpress]: Loaded")
 
@@ -20,4 +21,5 @@ func _on_button_activate(button):
 	print("button default booped: ", button)
 
 func _on_loadscene_request(scenepath):
-	assert(get_tree().change_scene(scenepath) == OK, "Failed to change scene on buttonpress")
+	if (get_tree().change_scene(scenepath) != OK):
+		push_error("Failed to change scene on buttonpress")
